c275b3b5e037981dc89b00ed4c1285f8234c9bca
Agung Prasasti Abadi <agungprasastia@gmail.com>
2026-07-07 08:58:13 +0800

    feat: add environment configuration files for production and development

---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  supabaseUrl: 'https://bsosdlnpmwwsexlvaeda.supabase.co',
+  supabaseAnonKey: 'sb_publishable_ZSoLpIozftC-apJoDX50ug_HjIaGG_I',
+  apiUrl: 'https://laundryapp-backend-production.up.railway.app/api',
+};
