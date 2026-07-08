e390c95955c75585acb74d05daa798b2f203b3d3
Agung Prasasti Abadi <agungprasastia@gmail.com>
2026-07-09 06:50:06 +0800

    feat: add development environment configuration and configure production file replacement in angular.json

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
   supabaseUrl: 'https://bsosdlnpmwwsexlvaeda.supabase.co',
   supabaseAnonKey: 'sb_publishable_ZSoLpIozftC-apJoDX50ug_HjIaGG_I',
-  apiUrl: 'https://laundryapp-backend-production.up.railway.app/api',
+  apiUrl: 'http://localhost:3000/api',
 };
